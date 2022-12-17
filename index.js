0f68110f2fd23355a4879059015963e2b8fcb1ed
Colton Brenneman <colton.brenneman@gmail.com>
2022-12-17 13:12:15 -0700

    commit tree 5ca129bddf856d84e0f228893cb9ea90c14cb70e

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+return customerName;
+
+function upperCaseCustomerName(){
+    customerName = customerName.toLocaleUpperCase();
+}
+
+setBestCustomer = () => {
+    bestCustomer = 'not bob';
+}
+
+overWriteBestCustomer = () => {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Brian';
+
+changeLeastFavoriteCustomer = () => {
+    return leastFavoriteCustomer = 'John';
+}
